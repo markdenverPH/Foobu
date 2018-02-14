70b673ff7dc56ef298fe4dd67e352ab64256e1d7
markdenverPH <markdenverbabaran@gmail.com>
2018-02-14 22:05:32 +0800

    login and reg
    -no functionalities yet
    -designs only
    -finished with login
    -ongoing reg desgin

--- app/src/main/java/com/example/babar/foobu/LoginActivity.java
+++ app/src/main/java/com/example/babar/foobu/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.babar.foobu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login_button);
+        register = findViewById(R.id.reg_button);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
